3642d7bec8044124ee4f3d44e4199f672a38e18f
Joseph Samuels <joseph.samuels@oswego.edu>
2019-04-10 16:22:19 -0400

    master - Added close button. Needs to implement action.

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,6 +2,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 // Load application packages.
 import './app';
